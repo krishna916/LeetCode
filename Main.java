f418701c344d7841563f59baa2e464ac511c903f
Krishnamurti Pandaram <krishna916@outlook.com>
2019-12-16 03:53:25 +0000

    added Hashmap solution

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Main {
   public static void main(String[] args) {
     int[] num = {5, 3, 7, 12, 43, 6};
-    int[] result = twoSum(num, 11);
+    // int[] result = twoSum(num, 11);
+    // for(int i =0; i< result.length; i++) {
+    //   System.out.print(result[i] + " ");
+    // }
+
+    int[] result = twoSumMap(num, 49);
     for(int i =0; i< result.length; i++) {
       System.out.print(result[i] + " ");
     }
@@ -35,4 +43,35 @@ class Main {
       }
       return result;
     }
+
+
+  /*
+   * Given an array of integers, return indices of the two numbers such that they add up to * a specific target.
+   * You may assume that each input would have exactly one solution, and you may not use * * the same element twice.
+   *
+   * Example:
+   *
+   * Given nums = [2, 7, 11, 15], target = 9,
+   *
+   * Because nums[0] + nums[1] = 2 + 7 = 9,
+   * return [0, 1].
+   * 
+   * Using hashmap O(n) imlementation
+   */
+  public static int[] twoSumMap(int[] nums, int target) {
+    int length = nums.length;
+    int [] result = new int[2];
+    Map<Integer, Integer> sums = new HashMap<>();
+    for(int i = 0; i < length; i++) {
+      if(sums.get(target - nums[i]) != null) {
+        result[0] = sums.get(target - nums[i]);
+        result[1] = i;
+      } else {
+        sums.put(nums[i], i);
+      }
+    }
+    return result;
+  }
+
+
 }
